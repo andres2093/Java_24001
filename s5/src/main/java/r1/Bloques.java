7204443ca1848f4fc7e39bddf3c71cae126749bc
Andrés Ramírez <andres200493@gmail.com>
2024-07-17 20:07:10 -0600

    S5 -> R1

---
+++ s5/src/main/java/r1/Bloques.java
@@ -0,0 +1,13 @@
+package r1;
+
+public class Bloques {
+    private static final int VALOR = 50;
+
+//    static {
+//        VALOR = 50;
+//    }
+
+    public static void muestraValor(){
+        System.out.println("valor: " + VALOR);
+    }
+}
---
+++ s5/src/main/java/r1/R1.java
@@ -0,0 +1,8 @@
+package r1;
+
+public class R1 {
+    public static void main(String[] args) {
+//        Bloques bloques = new Bloques();
+        Bloques.muestraValor();
+    }
+}
