f71099c5d27bd2a30719740444ff61c81346be2d
Andrés Ramírez <andres200493@gmail.com>
2024-07-24 21:01:55 -0600

    S7 -> E2

---
+++ s7/src/main/java/e2/Persona.java
@@ -0,0 +1,33 @@
+package e2;
+
+public class Persona implements Comparable<Persona>{
+    private final String nombre, apellido;
+    private float estatura;
+
+    public Persona(String nombre, String apellido, float estatura) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.estatura = estatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public String mostrarDatos(){
+        return nombre + " " + apellido + ": " + estatura;
+    }
+
+    @Override
+    public int compareTo(Persona persona) {
+        return Float.compare(this.estatura, persona.getEstatura());
+    }
+}
