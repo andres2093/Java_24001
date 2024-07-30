acb9a94d9363b2f86679bf3dcbda1f9f2876c38d
Andrés Ramírez <andres200493@gmail.com>
2024-07-29 20:25:15 -0600

    S8 -> E2

--- s8/src/main/java/e2/Texto.java
+++ s8/src/main/java/e2/Texto.java
@@ -1,2 +1,24 @@
-package e2;public class Texto {
+package e2;
+
+import java.io.*;
+
+public class Texto {
+    public static void main(String[] args) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.home") + "/archivo.txt"));
+            bw.write("Esta es la primer línea de mi documento");
+            bw.newLine();
+            bw.write("Esta es la segunda línea de mi documento\n");
+            bw.write("Esta es la tercera línea de mi documento");
+            bw.close();
+
+            BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.home") + "/archivo.txt"));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("El archivo no existe: " + e.getMessage());
+        }
+    }
 }
